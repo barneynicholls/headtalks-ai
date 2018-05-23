0565f5d57f40216f6971faa7320efc69869b2329
Barney Nicholls <barney.nicholls@bluetoad.net>
2018-05-23 14:08:07 +0100

    slowly slowly

--- headtalks/Pidentity/main.py
+++ headtalks/Pidentity/main.py
@@ -1,5 +1,6 @@
 import logging
 import logging.handlers
+import tempfile
 from tkinter import *
 from PIL import ImageTk, Image
 
@@ -12,47 +13,62 @@ cam = camera()
 window = Tk() 
 panel = Label(window)
 
-def snap():
-    cam.capture("trump.jpg")
-    snapped = ImageTk.PhotoImage(Image.open("trump.jpg"))
+def open_image(path):
+    snapped = ImageTk.PhotoImage(Image.open(path))
     panel.configure(image=snapped)
     panel.image_names=snapped
+
+def capture():
+    f = tempfile.NamedTemporaryFile(delete=False)
+    logging.info('capturing file: '+f.name)
+    cam.capture(f.name)
+    open_image("trump.jpg")
+
+def stop_preview():
+    cam.stop_preview()
+
+def start_preview():
+    cam.start_preview()
 
 def close():
     sys.exit()
 
-def main():
+def init():
     logging.basicConfig(level=logging.DEBUG)
     root = logging.getLogger()
-    # application log
     h = logging.handlers.RotatingFileHandler('pidentity.log', 'a', (1024 * 1024 * 2), 10)
     f = logging.Formatter('%(asctime)s %(processName)-10s %(name)s %(levelname)-8s %(message)s')
     h.setFormatter(f)
     root.addHandler(h)
     logging.info('Started')
 
-
+def default_ui():
     window.attributes('-fullscreen', True)
     window.configure(background = 'white')
 
     header = Label(window, text="Welcome to Headtalks",font=("Arial", 50), background='white')
     header.grid(column=0, row=0)
 
-    path = "putin.jpg"
-    img = ImageTk.PhotoImage(Image.open(path))
-
-
-    panel.configure(image=img)
+    open_image("putin.jpg")
     panel.grid(column=0, row=1)
 
     request = Label(window, text="Please tap your card and look at the camera",font=("Arial", 50), background='white')
     request.grid(column=0, row=2)
 
-    snapIt = Button(window,text="Snap", command=snap)
-    snapIt.grid(column=0,row=3)
+    buttonFrame = Frame(window,background = 'white')
+    buttonFrame.grid(column=0,row=3)
 
-    closeIt = Button(window,text="Close", command=close)
-    closeIt.grid(column=0,row=4)
+    cam_capture = Button(buttonFrame,text="Capture", command=capture)
+    cam_capture.grid( padx=10,column=0,row=0)
+
+    cam_startPreview = Button(buttonFrame,text="Start Preview", command=start_preview)
+    cam_startPreview.grid(padx=10,column=1,row=0)
+
+    cam_stopPreview = Button(buttonFrame,text="Stop Preview", command=stop_preview)
+    cam_stopPreview.grid(padx=10,column=2,row=0)
+
+    closeIt = Button(buttonFrame,text="Close", command=close)
+    closeIt.grid(padx=10,column=3,row=0)
 
     window.columnconfigure(0, weight=1)
     window.rowconfigure(0, weight=1)
@@ -61,6 +77,7 @@ def main():
     window.rowconfigure(3, weight=1)
     window.rowconfigure(4, weight=1)
 
+def main():
     window.mainloop()
 
     #app = App(title="Headtalks")
@@ -73,6 +90,8 @@ def main():
 
 if __name__ == "__main__":
     try:
+        init()
+        default_ui()
         main()
     except KeyboardInterrupt:
         pass
