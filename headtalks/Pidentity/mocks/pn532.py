53b48159056390c3ab759f82a8db342078fe1f9f
barneynicholls <barney.nicholls@bluetoad.net>
2018-05-25 16:28:18 +0100

    update mock

--- headtalks/Pidentity/mocks/pn532.py
+++ headtalks/Pidentity/mocks/pn532.py
@@ -4,7 +4,7 @@ class MockPN532Module(object):
     def __init__(self):
         self.mock = True;
 
-    def PN532(self,cs, sclk, mosi, miso):
+    def PN532(cs, sclk, mosi, miso):
         logging.debug("MockPN532Module create MockPN532")
         return MockPN532()
 
@@ -18,4 +18,8 @@ class MockPN532(object):
     def read_passive_target(self):
         logging.debug("MockPN532 read_passive_target")
         return None
+
+    def begin(self):
+        logging.debug("MockPN532 begin")
+       
 
